2c90a5df5ad4895edb6ee4e8939c040d13eee0e3
JeremySenn01 <jeremy.senn20801@gmail.com>
2018-12-10 08:55:21 +0100

    Implemented methods to calculate sum and difference

--- src/main/java/bbw/ch/consoleCalculatorWithMaven/Calculator.java
+++ src/main/java/bbw/ch/consoleCalculatorWithMaven/Calculator.java
@@ -2,4 +2,11 @@ package bbw.ch.consoleCalculatorWithMaven;
 
 public class Calculator {
 
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+	public int differenz(int minuend, int subtrahend) {
+		return minuend - subtrahend;
+	}
+	
 }
