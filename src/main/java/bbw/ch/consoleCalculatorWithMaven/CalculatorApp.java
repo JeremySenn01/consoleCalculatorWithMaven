cce279489e565712f8c7c4fb7368aa0465934ff6
JeremySenn01 <jeremy.senn20801@gmail.com>
2018-12-10 08:59:33 +0100

    Implemented method calls

--- src/main/java/bbw/ch/consoleCalculatorWithMaven/CalculatorApp.java
+++ src/main/java/bbw/ch/consoleCalculatorWithMaven/CalculatorApp.java
@@ -4,5 +4,18 @@ public class CalculatorApp {
 
 	public static void main(String[] args) {
 		
+		Calculator calc = new Calculator();
+		int valueA = 0;
+		int valueB = 0;
+		
+		System.out.println("Console Calculator");
+		System.out.println("******************");
+	
+		valueA = 10;
+		valueB = 20;
+		
+		System.out.println("Summe " + valueA + " + " +  valueB + " = " + calc.summe(valueA, valueB));
+		System.out.println("Differenz " + valueA + " - " +  valueB + " = " + calc.differenz(valueA, valueB));
+
 	}
 }
