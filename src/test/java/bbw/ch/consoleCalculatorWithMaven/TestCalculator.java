05f5cefefa5fb8bc92dd8cdd1bdcc32a8f28647e
5ia17jesenn <5ia17jesenn@gibwintra.ch>
2018-12-10 11:00:38 +0100

    Implement function to test positive sum

--- src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
+++ src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
@@ -2,13 +2,24 @@ package bbw.ch.consoleCalculatorWithMaven;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculator {
 
-	@Test
-	public void test() {
-		assertTrue(true);
+	private Calculator testee;
+	
+	@Before
+	public void setUp() {
+		testee = new Calculator();
 	}
+	
+	@Test
+	public void test_summe_zweiPositive_OK() {
+		assertTrue(testee.summe(4, 10) == 14);
+		
+	}
+	
+	
 
 }
