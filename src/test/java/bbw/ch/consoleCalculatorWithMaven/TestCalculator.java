b965ade7a04f89ac09ed8e47e9e70274b49598be
JeremySenn01 <jeremy.senn20801@gmail.com>
2018-12-17 08:28:16 +0100

    Implemented formula to test division divisor is zero

--- src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
+++ src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
@@ -8,51 +8,56 @@ import org.junit.Test;
 public class TestCalculator {
 
 	private Calculator testee;
-	
+
 	@Before
 	public void setUp() {
 		testee = new Calculator();
 	}
-	
+
 	@Test
 	public void test_summe_zweiPositive_OK() {
 		assertTrue(testee.summe(4, 10) == 14);
-		
+
 	}
-	
+
 	@Test
 	public void test_differenz_zweiPositive_OK() {
 		assertTrue(testee.differenz(4, 4) == 0);
-				
+
 	}
-	
+
 	@Test
 	public void test_summe_zweiNegative_OK() {
 		assertTrue(testee.summe(-3, -10) == -13);
 	}
-	
+
 	@Test
 	public void test_difference_zweiNegative_OK() {
 		assertTrue(testee.differenz(-3, -10) == 7);
 	}
-	
+
 	@Test
 	public void test_multiply_zweiPositive_OK() {
 		assertTrue(testee.multiply(5, 10) == 50);
 	}
-	
+
 	@Test
 	public void test_multiply_zweiNegative_OK() {
 		assertTrue(testee.multiply(-5, -10) == 50);
 	}
-	
+
 	@Test
 	public void test_divide_zweiPositive_OK() {
 		assertTrue(testee.divide(10, 2) == 5);
 	}
-	
+
 	@Test
 	public void test_divide_zweiNegative_OK() {
 		assertTrue(testee.divide(-10, -2) == 5);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void test_divide_divisorIsZero_NOK() {
+		testee.divide(10, 0);
 	}
 }
