9285eac2534827bbe85b8498538f1cc2398128c5
JeremySenn01 <jeremy.senn20801@gmail.com>
2018-12-17 08:13:10 +0100

    post merge

--- src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
+++ src/test/java/bbw/ch/consoleCalculatorWithMaven/TestCalculator.java
@@ -2,13 +2,39 @@ package bbw.ch.consoleCalculatorWithMaven;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculator {
 
-	@Test
-	public void test() {
-		assertTrue(true);
+	private Calculator testee;
+	
+	@Before
+	public void setUp() {
+		testee = new Calculator();
 	}
+	
+	@Test
+	public void test_summe_zweiPositive_OK() {
+		assertTrue(testee.summe(4, 10) == 14);
+		
+	}
+	
+	@Test
+	public void test_differenz_zweiPositive_OK() {
+		assertTrue(testee.differenz(4, 4) == 0);
+				
+	}
+	
+	@Test
+	public void test_summe_zweiNegative_OK() {
+		assertTrue(testee.summe(-3, -10) == -13);
+	}
+	
+	@Test
+	public void test_difference_zweiNegative_OK() {
+		assertTrue(testee.differenz(-3, -10) == 7);
+	}
+	
 
 }
