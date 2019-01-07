a61620437874835c185450c1b3b1fe21c21f3c5a
5ia17jesenn <5ia17jesenn@gibwintra.ch>
2019-01-07 09:12:15 +0100

    implemented all tests and modified calculator methods

--- src/main/java/bbw/ch/consoleCalculatorWithMaven/Calculator.java
+++ src/main/java/bbw/ch/consoleCalculatorWithMaven/Calculator.java
@@ -3,10 +3,23 @@ package bbw.ch.consoleCalculatorWithMaven;
 public class Calculator {
 
 	public int summe(int summand1, int summand2) {
+		
+		long result = (long) summand1 + (long) summand2;
+		
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+			throw new ArithmeticException();
+		}
 		return summand1 + summand2;
 	}
 
 	public int differenz(int minuend, int subtrahend) {
+		
+		long result = (long) minuend - (long) subtrahend;
+		
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+			throw new ArithmeticException();
+		}
+		
 		return minuend - subtrahend;
 	}
 
@@ -15,6 +28,15 @@ public class Calculator {
 	}
 
 	public int divide(int dividend, int divisor) {
+		
+		long result = (long) dividend / (long) divisor;
+		
+		
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+			System.out.println("invalid result");
+			throw new ArithmeticException();
+		}
+		
 		if (divisor == 0) {
 			throw new ArithmeticException();
 		}
